7658d3746fe82551d88e716813bcc889aea226bc
Alexuber <alexuberbuber@gmail.com>
2023-05-27 16:45:45 +0300

    finish with addHeroForm

--- src/components/HeroForm/HeroForm.jsx
+++ src/components/HeroForm/HeroForm.jsx
@@ -1,17 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Radio,
-  Select,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Button } from '@mui/material';
 import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -22,28 +10,41 @@ const validationSchema = Yup.object({
     .min(3, '3 characters minimum')
     .max(30, '30 characters maximum')
     .matches(regEx, 'English letters only')
-    .required()
-    .label('Nickname'),
+    .required('Nickname is a required field'),
   real_name: Yup.string()
     .min(3, '3 characters minimum')
     .max(30, '30 characters maximum')
     .matches(regEx, 'English letters only')
-    .required()
-    .label('Real name'),
+    .required('Real name is a required field'),
   origin_description: Yup.string()
     .min(3, '3 characters minimum')
     .max(300, '300 characters maximum')
     .matches(regEx, 'English letters only')
-    .required()
-    .label('Description'),
-  superpowers: Yup.array().of(Yup.string().label('Superpower')),
+    .required('Description is a required field'),
+  superpowers: Yup.array()
+    .of(
+      Yup.string()
+        .min(3, '3 characters minimum')
+        .max(30, '30 characters maximum')
+        .matches(regEx, 'English letters only')
+        .label('Superpower')
+    )
+    .required('Superpowers are required')
+    .min(1, 'Please add at least one superpower'),
   images: Yup.array()
     .of(
-      Yup.mixed().test('fileFormat', 'Invalid file format', value => {
+      Yup.mixed().test('fileFormat', 'Invalid file format', function (value) {
         if (value && value.length > 0) {
           for (let i = 0; i < value.length; i++) {
             const file = value[i];
-            if (!file.type.match(/^image\//)) {
+            const supportedFormats = [
+              'image/jpeg',
+              'image/jpg',
+              'image/png',
+              'image/webp',
+            ];
+            const isValidFormat = supportedFormats.includes(file.type);
+            if (!isValidFormat) {
               return false;
             }
           }
@@ -51,6 +52,7 @@ const validationSchema = Yup.object({
         return true;
       })
     )
+    .min(1, 'Please select at least one image')
     .required('Please select at least one image'),
 });
 
@@ -86,37 +88,71 @@ const HeroForm = () => {
       onSubmit={handleSubmit}
     >
       {({ values, setFieldValue }) => (
-        <Form>
-          <div>
+        <Form
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            maxWidth: '400px',
+          }}
+        >
+          <div style={{ marginBottom: '1rem' }}>
             <label htmlFor="nickname">Nickname</label>
-            <Field type="text" id="nickname" name="nickname" />
-            <ErrorMessage name="nickname" component="div" />
+            <Field
+              type="text"
+              id="nickname"
+              name="nickname"
+              style={{ width: '100%', padding: '0.5rem' }}
+            />
+            <ErrorMessage
+              name="nickname"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
-          <div>
+          <div style={{ marginBottom: '1rem' }}>
             <label htmlFor="real_name">Real Name</label>
-            <Field type="text" id="real_name" name="real_name" />
-            <ErrorMessage name="real_name" component="div" />
+            <Field
+              type="text"
+              id="real_name"
+              name="real_name"
+              style={{ width: '100%', padding: '0.5rem' }}
+            />
+            <ErrorMessage
+              name="real_name"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
-          <div>
+          <div style={{ marginBottom: '1rem' }}>
             <label htmlFor="origin_description">Description</label>
             <Field
               as="textarea"
               id="origin_description"
               name="origin_description"
+              style={{ width: '100%', padding: '0.5rem' }}
             />
-            <ErrorMessage name="origin_description" component="div" />
+            <ErrorMessage
+              name="origin_description"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
-          <div>
+          <div style={{ marginBottom: '1rem' }}>
             <label>Superpowers</label>
             <FieldArray name="superpowers">
               {({ push, remove, form }) => (
                 <div>
                   {form.values.superpowers.map((_, index) => (
                     <div key={index}>
-                      <Field name={`superpowers[${index}]`} />
-                      <button type="button" onClick={() => remove(index)}>
-                        Remove
-                      </button>
+                      <Field
+                        name={`superpowers[${index}]`}
+                        style={{ width: '100%', padding: '0.5rem' }}
+                      />
+                      {index > 0 && (
+                        <button type="button" onClick={() => remove(index)}>
+                          Remove
+                        </button>
+                      )}
                     </div>
                   ))}
                   <button type="button" onClick={() => push('')}>
@@ -125,23 +161,35 @@ const HeroForm = () => {
                 </div>
               )}
             </FieldArray>
-            <ErrorMessage name="superpowers" component="div" />
+            <ErrorMessage
+              name="superpowers"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
-          <div>
+          <div style={{ marginBottom: '1rem' }}>
             <label htmlFor="images">Images</label>
             <input
               type="file"
               id="images"
               name="images"
+              accept=".jpg, .jpeg, .png, .webp"
               multiple
               onChange={event => {
                 const fileList = Array.from(event.target.files);
                 setFieldValue('images', fileList);
               }}
+              style={{ padding: '0.5rem' }}
             />
-            <ErrorMessage name="images" component="div" />
+            <ErrorMessage
+              name="images"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
-          <Button type="submit">Submit</Button>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
         </Form>
       )}
     </Formik>
